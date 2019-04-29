71c4dd1e4f17fa2ea9e31e6827884fa2506891da
System Administrator <root@MacBook-Air-de-Reinaldo-2.local>
2019-04-29 00:10:20 -0400

    mi primer commit

---
+++ Basic/01-First-app/server.js
@@ -0,0 +1,17 @@
+const http = require('http'); //Importa el HTTP
+const fs = require('fs'); // Importa Fs para leer archivos
+
+function OnRequest(request, response){
+    response.writeHead(200,{'Content-type':'text/html'});
+    fs.readFile('./index.hbs',null, function(error,data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('File not found');
+        } else {
+            response.write(data);
+        }
+
+        response.end();
+    });
+    }
+http.createServer(OnRequest).listen(8000)
